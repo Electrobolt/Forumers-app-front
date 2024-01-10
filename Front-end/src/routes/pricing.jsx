05dfe290551ae414ef0963780bbfa5844c465462
Arhychem <ananonyman@hotmail.com>
2024-01-10 01:41:54 +0100

    fixing routing bug Component

---
+++ Front-end/src/routes/pricing.jsx
@@ -0,0 +1,31 @@
+import styles from "../styleSheets/pricing.module.css";
+import PricingItem from "../components/PricingItem.jsx";
+import { useAuth0 } from "@auth0/auth0-react";
+const Pricing = () => {
+  const { isAuthenticated } = useAuth0();
+  return (
+    <div className={styles["pricing-container"]}>
+      {!isAuthenticated && (
+        <PricingItem
+          type="FREE"
+          text="Run your blog and side projects on ngrok free forever"
+          content_button="Get Started"
+          link="/account"
+        />
+      )}
+      <PricingItem
+        type="PREMIUM"
+        text="Deliver globally available high-performance apps and APIs"
+        content_button="Subscribe"
+        link="/account"
+      />
+      <PricingItem
+        type="VIP"
+        text="For businesses with additional security and support needs"
+        content_button="Subscribe"
+        link="/account"
+      />
+    </div>
+  );
+};
+export default Pricing;
